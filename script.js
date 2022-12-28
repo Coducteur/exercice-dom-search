fd7c8ae8b0125fd9bdfcb9d22adecf9c9c1ef38b
AminaChakirDev <amina.aitm@gmail.com>
2022-12-28 11:13:14 +0100

    Exercice dom search filter

---
+++ script.js
@@ -0,0 +1,33 @@
+// Créer un tableau d'objet contenant les informations sur les films à afficher
+const movies = [
+
+];
+
+// Créer la fonction qui affichera les films
+function displayMovies(moviesArray) {
+  moviesArray.map((movie) => {
+    //Je crée le conteneur de la fiche du film
+
+    //Je crée le conteneur du titre du film
+
+    //Je crée le conteneur de l'affiche du film
+
+    //Je crée le conteneur de la liste des acteurs du film
+  });
+}
+
+//J'appelle la fonction
+
+
+
+
+// GESTION DU FORMULAIRE
+
+// Je crée l'écouteur d'évènement à la soumission du formulaire
+
+  // Je réinitialise l'affichage en supprimant les films déjà affichés
+
+  //Je crée un tableau ne contenant que les films répondant au critère défini (titre)
+  
+  // J'appelle la fonction d'affichage
+
