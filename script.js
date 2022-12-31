8b181e7d31e09f32eb55f4fb63957c179ff0a268
Coducteur <chat.adr01@gmail.com>
2022-12-31 15:47:05 +0100

    create array of films

--- script.js
+++ script.js
@@ -1,7 +1,94 @@
 // Créer un tableau d'objet contenant les informations sur les films à afficher
 const movies = [
-
+  { title : "Avatar",
+    actors: [
+      "Sam Worthington", 
+      "Zoe Saldana", 
+      "Sigourney Weaver"
+    ],
+    genre: "science-fiction",
+    poster: "./assets/avatar.jpg"
+  },
+  { title : "Avengers",
+    actors: [
+      "Robert Downey J", 
+      "Chris Evans", 
+      "Mark Ruffalo", 
+      "Chris Hemsworth", 
+      "Scarlett Johannson"
+    ],
+    genre: "fantastique",
+    poster: "./assets/avengers.jpeg"
+  },
+  { title : "Elvis",
+    actors: [
+      "Austin Butler", 
+      "Tom Hanks"
+    ],
+    genre: "biographie",
+    poster: "./assets/elvis.jpeg"
+  },
+  { title : "Harry Potter",
+    actors: [
+      "Daniel Radcliffe",
+      "Emma Watson",
+      "Rupert Grint"
+    ],
+    genre: "fantastique",
+    poster: "./assets/harry-potter.jpg"
+  },
+  { title : "Harry Potter suite",
+    actors: [
+      "Daniel Radcliffe",
+      "Emma Watson",
+      "Rupert Grint"
+    ],
+    genre: "fantastique",
+    poster: "./assets/harry-potter-2.jpeg"
+  },
+  { title : "Harry Potter Final",
+    actors: [
+      "Daniel Radcliffe",
+      "Emma Watson",
+      "Rupert Grint"
+    ],
+    genre: "fantastique",
+    poster: "./assets/harry-potter-final.jpeg"
+  },
+  { title : "Joker",
+    actors: [
+      "Joaquin Phoenix", 
+    ],
+    genre: "thriller",
+    poster: "./assets/joker.jpeg"
+  },
+  { title : "Matrix",
+    actors: [
+      "Keanu Reeves", 
+      "Laurence Fishburne", 
+    ],
+    genre: "science-fiction",
+    poster: "./assets/matrix.jpeg"
+  },
+  { title : "The Notebook",
+    actors: [
+      "Ryan Gosling", 
+      "Rachel McAdams", 
+    ],
+    genre: "romance",
+    poster: "./assets/notebook.jpeg"
+  },
+  { title : "Titanic",
+    actors: [
+      "Leonardo DiCaprio", 
+      "Kate Winslet", 
+  ],
+    genre: "romance",
+    poster: "./assets/titanic.jpg"
+  },
 ];
+
+
 
 // Créer la fonction qui affichera les films
 function displayMovies(moviesArray) {
